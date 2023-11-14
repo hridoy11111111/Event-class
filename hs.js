89540f730c810dc5cc3a496dc5fc6877b9977dc6
HRIDOY CHANDRA  SAHA <145109607+hridoy11111111@users.noreply.github.com>
2023-11-14 11:14:00 +0600

    hs

---
+++ hs.js
@@ -0,0 +1,34 @@
+const changeButton = document.getElementById('change');
+changeButton.addEventListener('click',function(){
+    document.body.style.innerText = 'Hello Everyone'
+})
+// console.log(click);
+
+function makered(){
+    document.body.style.backgroundColor = 'red'
+}
+
+function makegreen(){
+    document.body.style.backgroundColor = 'green'
+}
+
+const blueButton = document.getElementById("make-blue");
+blueButton.onclick = makeblue;
+function makeblue() {
+    document.body.style.backgroundColor = 'blue'
+}
+// console.log(blueButton);
+
+const aquaButton = document.getElementById("make-aqua");
+aquaButton.onclick = function(){
+    document.body.style.backgroundColor = 'aqua'
+}
+
+const purpleButton = document.getElementById("purple");
+purpleButton.addEventListener('click',function(){
+    document.body.style.backgroundColor = 'purple'
+})
+
+document.getElementById("black").addEventListener('click',function(){
+    document.body.style.backgroundColor = 'black'
+})
